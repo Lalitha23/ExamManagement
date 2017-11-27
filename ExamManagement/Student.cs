23e73e08ef981f184f78eb8554c31ae3078ecdd8
Lalitha Pammi <lalitha.pammi@live.com>
2017-11-26 16:01:22 -0800

    Adding Student and Exam class

---
+++ ExamManagement/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamManagement
+{
+    /// <summary>
+    /// Student Class
+    /// </summary>
+    enum Level
+    {
+        Level1,
+        Level2,
+        Level3,
+        Level4
+    };
+
+    class Student
+    {
+        #region Properties
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int MinimumAge { get; private set; }
+        public Level StudentLevel { get; set; }
+#endregion
+
+        #region Statics
+        private static int age;
+#endregion
+
+        #region Constructor
+        public Student()
+        {
+            MinimumAge = 6;
+        }
+        #endregion
+
+        #region Methods
+        //Methods need to be defined
+#endregion
+    }
+}
